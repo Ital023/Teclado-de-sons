e549d8b9f495e8b5183534cfa72d1f530e806769
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-03-07 21:35:28 -0300

    inserindo o projeto

---
+++ main.js
@@ -0,0 +1,28 @@
+function tocaSom(idElementoSom){
+    document.querySelector(idElementoSom).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let cont = 0;
+
+
+for(let cont = 0; cont < listaDeTeclas.length;cont++){
+    const tecla = listaDeTeclas[cont];
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+
+        tecla.onkeydown = function (){
+            tecla.classList.add('ativa');
+        }
+
+        tecla.onkeyup = function(){
+            tecla.classList.remove('ativa');
+        }
+    }
+}
+
